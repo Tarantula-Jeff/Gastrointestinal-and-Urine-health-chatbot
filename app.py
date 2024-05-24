bdda8c239832a623e50cad57a8747cffca0df4ff
Tarantula-Jeff <113800958+Tarantula-Jeff@users.noreply.github.com>
2024-05-24 18:38:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,12 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-     embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+     embeddings = OpenAIEmbeddings()
      vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
      return vectorstore
 
 def get_conversation_chain(vectorstore):
-   llm = ChatOpenAI(openai_api_key=api_key)
+   llm = ChatOpenAI()
    memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
    conversation_chain = ConversationalRetrievalChain.from_llm(
