9e3c02ccffcdec578f84e9c8f471dc26b03d7e88
Tarantula-Jeff <113800958+Tarantula-Jeff@users.noreply.github.com>
2024-05-24 19:13:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
 from htmlTemplates import css, bot_template, user_template
 
-api_key = os.getenv("OPENAI_API_KEY")
+
+
 def get_pdf_text():
      relative_pdf_path = "FeedDocs/Urine and poop health.pdf"
      text = ""
