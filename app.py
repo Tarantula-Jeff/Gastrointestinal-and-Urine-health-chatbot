0259c02d6dc14a8ebcbc990f461cc54805ae28dc
Tarantula-Jeff <113800958+Tarantula-Jeff@users.noreply.github.com>
2024-05-24 14:58:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain_community.embeddings import OpenAIEmbeddings
 from htmlTemplates import css, bot_template, user_template
 
-api_key =os.getenv('OPENAI_API_KEY')
+
 def get_pdf_text():
      relative_pdf_path = "FeedDocs/Urine and poop health.pdf"
      text = ""
@@ -32,12 +32,12 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+     embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks,embedding=embeddings)
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-   llm = ChatOpenAI(api_key=api_key)
+   llm = ChatOpenAI()
    memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
    conversation_chain = ConversationalRetrievalChain.from_llm(
@@ -68,6 +68,7 @@ def handle_userinput(user_question):
 
 
 def main():
+     api_key =os.getenv('OPENAI_API_KEY')
     
     st.set_page_config(page_title="Sturine Chatbot", page_icon=":robot_face:")
     st.write(css, unsafe_allow_html=True)
