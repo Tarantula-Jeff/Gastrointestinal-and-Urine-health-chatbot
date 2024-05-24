e9e0bcda670e3e3d495581025834920f37bb19c5
Tarantula-Jeff <113800958+Tarantula-Jeff@users.noreply.github.com>
2024-05-24 18:01:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain_community.chat_models import ChatOpenAI
 from langchain_community.embeddings import OpenAIEmbeddings
 from htmlTemplates import css, bot_template, user_template
 
